10e5aaa3a357b5cd922a39ff2a549da008437664
prithviraj324 <pritvirajc.cs19@bmsce.ac.in>
2022-04-27 20:00:54 +0530

    #1. Two sum

--- twosum.cpp
+++ twosum.cpp
@@ -1,8 +1,19 @@
-#include<bits/stdc++.h>
+#include<bits/stdc++.h>;
 using namespace std;
-int main() {
-    int a;
-    cin>>a;
-    cout<<"hello";
-    return 0;
-}
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> soln={-1,-1};
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++) {
+            int comp=target-nums[i];
+            if(m.find(comp)==m.end()) 
+                m.insert({nums[i],i});        
+            else if(m.find(comp)!=m.end()) {
+                soln[0]=m[comp];
+                soln[1]=i;
+            }
+        }
+        return soln;
+    }
+};
