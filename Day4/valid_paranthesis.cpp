0a1850c6a76b9ac6fabf8f1aa4653d6192d4e221
prithviraj324 <pritvirajc.cs19@bmsce.ac.in>
2022-05-03 15:18:20 +0530

    20. Valid Parentheses

---
+++ Day4/valid_paranthesis.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto c:s) {//using normal for loop increases time taken by 500%
+            if(c=='(' || c=='{' || c=='[')
+                st.push(c);
+            else if(!st.empty()){
+                if(c==')' && st.top()=='(')
+                    st.pop();
+                else if(c=='}' && st.top()=='{')
+                    st.pop();
+                else if(c==']' && st.top()=='[')
+                    st.pop();
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+        if(st.empty())
+            return true;
+        else
+            return false;
+    }
+};
