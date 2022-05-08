4ffae03f071cc57c5e10b90aeed750e4c41111b3
prithviraj324 <pritvirajc.cs19@bmsce.ac.in>
2022-05-08 23:17:47 +0530

    104. Maximum Depth of Binary Tree

--- Day7/bt_max_depth.cpp
+++ Day7/bt_max_depth.cpp
@@ -9,6 +9,11 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+//-------------------------------------------------------------
+//naive recursive solution
+//tc-O(n), we go through all nodes in bt
+//sc-O(n): each node in recursive call stack
+//---------------------------------------------------------------- 
 class Solution {
 public:
     int inorder(TreeNode* root, int depth) {
@@ -23,3 +28,15 @@ public:
         return inorder(root,depth);
     }
 };
+//----------------------------------------------------------------
+//using level order traversal, you can do it iteratively 
+//tc-O(n): going through all nodes in the tree
+//sc-O(logn)->O(n): since we can have either max logn nodes in queue(in case of balanced bt) or n nodes (in case of unbalanced bt)
+//----------------------------------------------------------------
+
+
+//----------------------------------------------------------------
+//another solution is iterative dfs using preorder traversal and stack
+//tc-O(n): same as before
+//sc-O(n): simulating recursive callstack as in first Solution
+//----------------------------------------------------------------
