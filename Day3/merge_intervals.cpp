b2af5b40e1d91ae25d6de64a11de5efea9b2ee77
prithviraj324 <pritvirajc.cs19@bmsce.ac.in>
2022-05-10 21:30:36 +0530

    56. Merge Intervals

--- Day3/merge_intervals.cpp
+++ Day3/merge_intervals.cpp
@@ -33,3 +33,22 @@ public:
         return res;
     }
 };
+
+//-------------------------------
+//a simple, compact, faster version of above code with same logic
+//-------------------------------
+
+class Solution2 {
+public:
+     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.size()<=1) return intervals;
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> output;
+        output.push_back(intervals[0]);
+        for(int i=1; i<intervals.size(); i++) {
+            if(output.back()[1] >= intervals[i][0]) output.back()[1] = max(output.back()[1],intervals[i][1]);
+            else output.push_back(intervals[i]); 
+        }
+        return output;
+     }
+};
