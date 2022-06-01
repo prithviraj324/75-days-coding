3029ccbd27f67eeaaaba0dbbd4c9f4b8be7847be
prithviraj324 <pritvirajc.cs19@bmsce.ac.in>
2022-06-01 20:18:11 +0530

    121. Best Time to Buy and Sell Stock

--- Day1/buy_and_sell_stock.cpp
+++ Day1/buy_and_sell_stock.cpp
@@ -1,16 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
+//2 ptrs, one is at end(right ptr) and the other is end-1(left ptr)
+//keep decrementing leftptr till u reach beginning of array
+//pricemax stores value of max price thus far seen in subarray [l,r]
+//at each step, calculate (leftptr - pricemax), ie buy at day leftptr and sell at day pricemax for max profit
+//update maxprofit and pricemax accordingly
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit=0,pmax=*(prices.rbegin()),pnew=-1;
+        int maxprofit=0, pricemax=*(prices.rbegin()), pnew=-1;
         for(auto i=prices.rbegin()+1; i!=prices.rend(); i++) {
-            pnew=pmax-(*i);
-            if(pnew>=profit)
-                profit=pnew;
-            if(pmax<*i)
-                pmax=*i; 
+            pnew = pricemax - (*i);
+            if(pnew >= maxprofit)
+                maxprofit = pnew;
+            if(pricemax < *i)
+                pricemax = *i;
         }
-        return profit;
+        return maxprofit;
     }
 };
