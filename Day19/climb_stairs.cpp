ac79d96465d6f88490e4f4a24d5deb7d603c9550
prithviraj324 <pritvirajc.cs19@bmsce.ac.in>
2022-06-05 01:51:37 +0530

    70. Climbing Stairs

--- Day19/climb_stairs.cpp
+++ Day19/climb_stairs.cpp
@@ -2,15 +2,13 @@
 using namespace std;
 class Solution {
 public:
-    
+    //memoized bottom up solution
     int steps(int n, vector<int>& v) {
-        if(v[n] != -1) return v[n];
-        if(n<=1)  return 1;
-        else if(n==2)
-            return steps(n-1, v) + steps(n-2, v);
-        else {
-            v[n-1] = steps(n-1, v);
-            v[n-2] = steps(n-2, v);
+        if(v[n] != -1) return v[n];//check if already memoized
+        if(n<=1)  return 1;//base condition
+        else {//recursive calls
+            v[n-1] = steps(n-1, v);//choice: take 1 step
+            v[n-2] = steps(n-2, v);//choice: take 2 steps
             return v[n-1]+v[n-2];
         }
     }
