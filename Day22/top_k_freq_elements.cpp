d52f6494b69d2a62562a10d1cc34273350167494
prithviraj324 <pritvirajc.cs19@bmsce.ac.in>
2022-06-11 02:46:35 +0530

    347. Top K Frequent Elements

--- Day22/top_k_freq_elements.cpp
+++ Day22/top_k_freq_elements.cpp
@@ -32,3 +32,28 @@ public:
         return v;
     }
 };
+//----------------------------------------------------------------
+//cool bucket sort Solution
+//TC: O(n), SC:O(n*_)->debatable
+//----------------------------------------------------------------
+class Solution2 {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> counts;
+        for(auto i : nums) ++counts[i];
+        
+        vector<vector<int>> buckets(nums.size() + 1);
+        for(auto & k : counts) 
+            buckets[k.second].push_back(k.first);
+        reverse(begin(buckets), end(buckets));
+        
+        vector<int> res;
+        for(auto & bucket: buckets) 
+            for(auto i : bucket) {
+                res.push_back(i);
+                if(res.size() == k) return res;
+            }
+        
+        return res;
+    }
+};
