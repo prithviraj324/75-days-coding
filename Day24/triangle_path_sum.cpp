abeccd4f3c6cf707127df3abb35f219d093deda1
prithviraj324 <pritvirajc.cs19@bmsce.ac.in>
2022-06-14 00:27:33 +0530

    120. Triangle

--- Day24/triangle_path_sum.cpp
+++ Day24/triangle_path_sum.cpp
@@ -18,21 +18,17 @@ public:
 //memoized top down->
 class Solution2 {
 public:
-    int minsum(vector<vector<int>>& tri, vector<vector<int>>& t, int n, int i, int j, int sum) {
-        if(i==n) return sum;
-        else {
-            sum+=tri[i][j];
-            int lchild=minsum(tri, t, n, i+1, j, sum);  //pathsum of left child
-            int rchild=minsum(tri, t, n, i+1, j+1, sum);    //pathsum of right child
-            t[i][j]=min(lchild, rchild);
-            return t[i][j];
-        }
+    int minsum(vector<vector<int>>& tri, vector<vector<int>>& t, int n, int i, int j) {
+        if(i==n-1) return tri[i][j];
+        if(t[i][j]!=-1) return t[i][j];
+        else
+            return t[i][j] = tri[i][j] +min(minsum(tri,t,n,i+1,j), minsum(tri,t,n,i+1,j+1));
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int r=triangle.size();
         int maxcols=r;  //read constraints
-        vector<vector<int>> t(r, vector<int>(r,-1)); //dp table
-        return minsum(triangle, t, r, 0, 0, 0);
+        vector<vector<int>> t(r, vector<int>(r, -1)); //dp table
+        return minsum(triangle, t, r, 0, 0);
     }
 };
 //top down tabulation->
