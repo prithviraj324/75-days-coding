fc8b0524f70c1943c7306f1e77453f16d408346e
prithviraj324 <pritvirajc.cs19@bmsce.ac.in>
2022-06-18 01:10:11 +0530

    138. Copy List with Random Pointer

--- Day28/copy_LL_randomptr.cpp
+++ Day28/copy_LL_randomptr.cpp
@@ -13,7 +13,7 @@ public:
         random = NULL;
     }
 };
-
+//retarded solution i thought of first, couldnt figure out relation between the new to old LL
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
@@ -51,5 +51,81 @@ public:
         }
         
         return res;
+    }
+};
+
+//----------------------------------------------------------------
+//think
+//no need to create new node AND link them
+//idea.jpg
+//pass 1: allocate space and create new nodes and only copy their values, dont link them yet
+//        store a old_node->new_node map
+//pass 2: consult map to find new_node of old_node, 
+//                  map[old] gives new
+//                  map[old]->next = map[old->next]
+//                  map[old]->random = map[old->random]
+
+class Solution2 {
+public:
+    Node* copyRandomList(Node* head) {
+        map<Node*, Node*> m;
+        int i=0;
+        Node* ptr = head;
+        while (ptr) {
+            m[ptr] =new Node(ptr->val);
+            ptr = ptr->next;
+        }
+        ptr = head;
+        while (ptr) {
+            m[ptr]->next = m[ptr->next];
+            m[ptr]->random = m[ptr->random];
+            ptr = ptr->next;
+        }
+        return m[head];
+    }
+};
+
+//O(1) space solution by adding a seperate pointer in each node     
+//https://leetcode.com/problems/copy-list-with-random-pointer/discuss/1059181/C%2B%2B-or-Three-Pass-or-O(n)-0ms-Beats-100-or-Explanation-(with-example)
+
+class Solution3 {
+public:
+    Node* copyRandomList(Node* head) {
+        
+        // STEP 1: PASS 1
+        // Creating a copy (except random pointer) of each old node and insert it next to the old node it's copied from.
+        // That is, it will create new alternative nodes which are a copy (except random pointer) of its previous node.
+        Node* node=head;
+        while(node){
+            Node* temp=node->next;
+            node->next=new Node(node->val);
+            node->next->next=temp;
+            node=temp;
+        }
+        
+        // STEP 2: PASS 2
+        // Now copy the random pointer (if exists) of the old nodes to their copy new nodes.
+        node=head;
+        while(node){
+            if(node->random)
+                node->next->random=node->random->next;
+            node=node->next->next; // go to next old node
+        }
+        
+        //STEP 3: PASS 3
+        // Copy the alternative nodes into "ans" link list using the "helper" pointer along with restoring the old link list.
+        Node* ans=new Node(0); // first node is a dummy node
+        Node* helper=ans;
+    
+        while(head){
+            // Copy the alternate added nodes from the old linklist
+            helper->next=head->next;
+            helper=helper->next;
+            
+            // Restoring the old linklist, by removing the alternative newly added nodes
+            head->next=head->next->next;
+            head=head->next; // go to next alternate node   
+        }
+        return ans->next; // Since first node is a dummy node
     }
 };
