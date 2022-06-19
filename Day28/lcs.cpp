1bcdf6e842979527cd09fcb0b9260e92929aefcf
prithviraj324 <pritvirajc.cs19@bmsce.ac.in>
2022-06-19 14:02:19 +0530

    1143. Longest Common Subsequence

--- Day28/lcs.cpp
+++ Day28/lcs.cpp
@@ -36,3 +36,25 @@ public:
         return solve(dp, s1, s2, n, m);
     }
 };
+//bottom up tabulated
+class Solution3 {
+public:
+    int longestCommonSubsequence(string s1, string s2)
+    {
+        int n=s1.size(), m=s2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+        for(int i=0; i<n+1; i++)
+            dp[i][0]=0;
+        for(int j=0;j<m+1;j++)
+            dp[0][j]=0;
+        for(int i=1;i<n+1;i++) {
+            for(int j=1;j<m+1;j++) {
+                if(s1[i-1]==s2[j-1]) 
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]); 
+            }
+        }
+        return dp[n][m];
+    }
+};
