35998ccc9d7ee4f5959f1fa85e79195bb4dfe0dc
prithviraj324 <pritvirajc.cs19@bmsce.ac.in>
2022-06-25 02:31:45 +0530

    78. Subsets

---
+++ Day33/subsets.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void dfs(vector<vector<int>>& ans, vector<int>& nums, int i, vector<int> x) {
+        if(i>=nums.size()) {
+            ans.push_back(x);
+            return;
+        }
+        dfs(ans, nums, i+1, x);
+        x.push_back(nums[i]);
+        dfs(ans, nums, i+1, x);
+        
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        dfs(ans, nums, 0, temp);
+        return ans;
+    }
+};
